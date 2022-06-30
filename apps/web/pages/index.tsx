382c79af48e30bbb6e6b0b2448ae12421bfad739
Greg Soltis <greg.soltis@vercel.com>
2022-06-30 10:53:01 -0700

    Thread a dependency to deep-ts-lib from web via my-js-lib

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Button } from "ui";
+import { getVeryImportantData } from "my-js-lib";
 
 export default function Web() {
+  const data = getVeryImportantData();
   return (
     <div>
-      <h1>Web</h1>
+      <h1>Web Data: {data}</h1>
       <Button />
     </div>
   );
---
+++ packages/my-js-lib/lib.js
@@ -0,0 +1,5 @@
+const f = require("deep-ts-lib");
+
+exports.getVeryImportantData = function () {
+  return "very " + f.getImportantData();
+};
